70fa7061a83ea36562214a7c8df8a73a56358e30
vincenzo <vincenzo.bolutti@gmail.com>
2020-07-07 22:58:24 +0200

    splash

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const repl = require('repl');
 const state=require("./modules/loadModules");
+console.info("");
+console.info("ETHEREUM MANAGEMENT CONSOLE (v0.5)");
+console.info("----------------------------------");
+console.info("(c) Vicente Boluda Vias 2020");
+console.info("");
+console.info("Platform: "+process.platform);
+console.info("Nodejs: "+process.version);
+console.info("");
 
 const myRepl = repl.start("Console $ ");
 
